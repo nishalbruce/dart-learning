aa39adfa7eea0f50130e5115ef830fc8bf91c561
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-03-13 11:25:53 +0200

    dart project created

---
+++ variables_and_data_types/bin/variables_and_data_types.dart
@@ -0,0 +1,5 @@
+import 'package:variables_and_data_types/variables_and_data_types.dart' as variables_and_data_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variables_and_data_types.calculate()}!');
+}
