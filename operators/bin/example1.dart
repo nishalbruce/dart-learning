19eb762bc8586a224be2aaeb327fd7b40aafa6e3
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-03-24 11:40:16 +0200

    file updated

---
+++ operators/bin/example1.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  const age1 = 64;
+  const age2 = 30;
+
+  print(age1 + age2);
+  print(age1 - age2);
+  print(age1 * age2);
+  print(age1 ~/ age2);
+  print(age1 % age2);
+}
--- operators/bin/operators.dart
+++
@@ -1,5 +0,0 @@
-import 'package:operators/operators.dart' as operators;
-
-void main(List<String> arguments) {
-  print('Hello world: ${operators.calculate()}!');
-}
