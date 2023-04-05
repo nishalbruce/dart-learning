2eee1f2512f58ef02e15af463bb8fb896d9e45a4
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-04-05 10:57:21 +0200

    addes some operators to the file

--- operators/bin/example1.dart
+++ operators/bin/example1.dart
@@ -2,9 +2,14 @@ void main(List<String> args) {
   const age1 = 64;
   const age2 = 30;
 
-  print(age1 + age2);
-  print(age1 - age2);
-  print(age1 * age2);
-  print(age1 ~/ age2);
-  print(age1 % age2);
+  print(age1 + age2); //Addition
+  print(age1 - age2); //Substarction
+  print(age1 * age2); //Multiplication
+  print(age1 / age2); //The result will be a double
+  print(age1 ~/ age2); // The result will be a integer
+  print(age1 % age2); //Modulo => the remainder
+
+  const age = 50;
+
+  print(age == 20); //=> false
 }
