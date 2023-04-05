a8691e3909ff32ff58c6645337ac7053c6e0c16c
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-04-05 12:05:08 +0200

    Added the optionals directory

---
+++ optionals/bin/example.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: unused_local_variable
+
+void main(List<String> args) {
+  int? age; // This is how we declare a nullable variable. This means that variable can either have a value or not
+}
