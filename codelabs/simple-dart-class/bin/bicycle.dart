fbbfce6dbb493da72881a21f26ed5ca15520a576
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-04-12 11:38:26 +0200

    Added the codelabs directory

---
+++ codelabs/simple-dart-class/bin/bicycle.dart
@@ -0,0 +1,9 @@
+class Bicycle {
+  int cadence;
+  int speed;
+  int gear;
+
+  Bicycle(this.cadence, this.speed, this.gear);
+}
+
+void main(List<String> args) {}
---
+++ codelabs/simple-dart-class/test/simple_dart_class_test.dart
@@ -0,0 +1,8 @@
+import 'package:simple_dart_class/simple_dart_class.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
