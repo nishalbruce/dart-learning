b07586686499952edff42a226fab275d2d2ad152
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-04-18 09:56:00 +0200

    example4 on functions modified

--- functions/bin/example4.dart
+++ functions/bin/example4.dart
@@ -5,9 +5,10 @@ void main() {
 
 void doSomethingWithAge({required int? age}) {
   if (age != null) {
-    final in2Years = age + 2;
+    final in2Years = age + 6;
     print("In 2 years, you'll be $in2Years years old.");
   } else {
     print("You didn't provide your age!");
   }
 }
+
