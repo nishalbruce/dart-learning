6d00e181b7cfa5d304b55512a06ee30ee6941bce
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-04-20 09:42:33 +0200

    Added the control flow directory

---
+++ control_flow/bin/control_flow.dart
@@ -0,0 +1,5 @@
+import 'package:control_flow/control_flow.dart' as control_flow;
+
+void main(List<String> arguments) {
+  print('Hello world: ${control_flow.calculate()}!');
+}
---
+++ control_flow/test/control_flow_test.dart
@@ -0,0 +1,8 @@
+import 'package:control_flow/control_flow.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
