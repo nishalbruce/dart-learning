5f1b08ed15f29976d53c470243a233eea5bc1a34
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-04-24 14:35:48 +0200

    Added some do-while statements

--- control_flow/bin/while_loop.dart
+++ control_flow/bin/while_loop.dart
@@ -7,6 +7,37 @@ void main(){
     counter++;
   }
 
-  
+  print("----------------");
+
+  counter = -1;
+  while (++counter < names.length) {
+    print(names[counter]);    
+  }
+
+  print("----------------");
+
+  counter = names.length;
+  while(--counter >= 0){
+    print(names[counter]);
+  }
+
+  print("----------------");
+
+  counter = 0;
+
+  do {
+    print(names[counter++]);
+  } while (counter < names.length);
+
+  print("----------------");
+
+  counter = 0;
+
+  do {
+    print(names[counter++]);
+    if (counter == 2) {
+      break;
+    }
+  } while (counter < names.length);
 
 }
