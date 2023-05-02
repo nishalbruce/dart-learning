37ccea729dfd446396169326ea67405bad073a73
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-05-02 10:26:16 +0300

    example file in classes dir modified

--- classes/bin/example.dart
+++ classes/bin/example.dart
@@ -1,3 +1,41 @@
-void main(List<String> args){
-  
+import "dart:io";
+
+void main(List<String> args) {
+  Person p = Person("Alain", "Bruce", 20);
+  p.showData();
+}
+
+class Person {
+  //Properties or attributes
+  String? name, sex;
+  int? age;
+
+  //Constructor
+  Person(this.name, this.sex, this.age);
+
+  ///The constructor above is simplified. It is the same thing like the example below
+  /// Person(String name, sex, int age){
+  ///  this.name = name;
+  ///  this.sex = sex;
+  ///  this.age = age;
+
+  //Method
+  void showData() {
+    print("Name = $name");
+    print("sex = $sex");
+    print("age = $age");
+  }
+
+  dynamic getData({String? name, String? sex, int? age}) {
+    stdout.write("Enter your name");
+    name = stdin.readLineSync();
+
+    stdout.write("Enter your sex");
+    sex = stdin.readLineSync();
+
+    stdout.write("Enter your age");
+    age = int.parse(stdin.readLineSync()!);
+
+    print("Your name is $name, your sex is $sex and your age is $age");
+  }
 }
